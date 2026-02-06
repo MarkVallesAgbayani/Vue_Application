8b765941f068784fad1c4394c6c1fc2cf36cc937
MarkVallesAgbayani <markagbayanipersonal19@gmail.com>
2026-02-06 11:32:53 +0800

    First Exercise

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import {ref} from 'vue';
+  import sampleImage from './assets/images/sample_image.webp';
+
+  const description = ref('This is a sample Vue component.');
+  const url = ref('https://www.youtube.com/');
+  const image = ref(sampleImage);
+
+</script>
+
+<template>
+  <p>Description: {{ description }}</p>
+  <a :href="url" target="_blank">Go to YouTube</a>
+
+  <div>
+    <p>This is image:</p>
+    <img :src="image" alt="Sample Image" />
+  </div>
+</template>
+
+<style scoped></style>
